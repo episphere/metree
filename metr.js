9579195c7c075d7012c80962e3e90ba262112ae8
Jonas Almeida <jalmeida@mathbiol.org>
2018-08-28 23:44:58 -0400

    coming soon

--- metr.js
+++ metr.js
@@ -4,7 +4,7 @@ metr=function(){
     // UI
     metr.div=document.body.querySelector('#meTreeDiv')
     if(metr.div){
-        metr.div.innerHTML='<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="25px"></a> <p style="color:green">is coming soon ...</p>'
+        metr.div.innerHTML='<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="25px"></a> <p style="color:green">coming soon ...</p>'
     }
 
 
