c4e92f3855304fbf855a25ef41d85d58b9a77432
Jonas Almeida <jalmeida@mathbiol.org>
2018-08-28 23:24:26 -0400

    onload metr

--- metr.js
+++ metr.js
@@ -1,1 +1,15 @@
-document.body.innerHTML='<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="25px"></a>'
+console.log('metr.js leaded');
+
+metr=function(){
+    // UI
+    metr.div=document.body.querySelector('#meTreeDiv')
+    if(metr.div){
+        metr.div.innerHTML='<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="25px"></a> is coming'
+    }
+
+
+}
+
+metr.date=Date()
+
+window.onload=metr
