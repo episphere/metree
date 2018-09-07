0f65617e352094899c4d9d5d94b936f7dda3c440
Jonas Almeida <jalmeida@mathbiol.org>
2018-09-07 10:26:56 -0400

    origal logo

--- metr.js
+++ metr.js
@@ -4,7 +4,10 @@ metr=function(){
     // UI
     metr.div=document.body.querySelector('#meTreeDiv')
     if(metr.div){
-        metr.div.innerHTML='<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="25px"></a> <p style="color:green">coming soon ...</p>'
+        var h = '<a href="https://github.com/episphere/metree" target="_blank"><img src="FHH_logo.png" height="40px"></a> <p style="color:green">coming soon ...</p>'
+        h +='API status <input>'
+        //h +='<textarea style="background-color:black"></textarea>'
+        metr.div.innerHTML=h
     }
 
 
