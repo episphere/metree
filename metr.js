e749ad0a35399721ee6c75c20ee7dc6f235fc99d
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-22 12:27:32 -0400

    v2 with onecowstanding

--- metr.js
+++ metr.js
@@ -1,76 +1,30 @@
-console.log('metr.js leaded');
+console.log('metr.js loaded')
 
 metr=function(){
-    // UI
-    metr.div=document.body.querySelector('#meTreeDiv')
+    // ini
+    metr.div=document.getElementById('meTreeDiv')
     if(metr.div){
-        var h = '<a href="https://github.com/episphere/metree" target="_blank"><img src="fhh_metreelogo.png" height="40px"></a> <p style="color:green">coming soon ...</p>'
-        h +='<button id="testAPIbutton">test API</button> <input id="testAPIinput" size="70" value="http://test.metree.lgs.littlegreensoftware.com:8888/family/load">'
-        h +='<div id="testResults"></div>'
-        metr.div.innerHTML=h
-        // events
-        var testAPIbutton=metr.div.querySelector('#testAPIbutton')
-        testAPIbutton.onclick=function(){
-            var testResults=metr.div.querySelector('#testResults')
-            testResults.innerHTML='<span style="color:blue">calling ... </span>'
-            var testAPIinput=metr.div.querySelector('#testAPIinput')
-            var url=testAPIinput.value
-            metr.get(url,function(x){
-                metr.json=x
-                testResults.innerHTML='[<span id="showHideJSON"style="color:orange;cursor:pointer">hide JSON</span>]<pre style="fontsize:small;color:green">'+JSON.stringify(x,null,3)+'</pre>'
-                showHideJSON.onclick=function(){
-                    //if(showHideJSON.text)
-                    if(this.textContent=="hide JSON"){
-                        this.textContent="show JSON"
-                        this.style.color="green"
-                        this.parentElement.querySelector('pre').hidden=true
-                    }else{
-                        this.textContent="hide JSON"
-                        this.style.color="orange"
-                        this.parentElement.querySelector('pre').hidden=false
-                    }
-                    //debugger
-                }
-                setTimeout(function(){
-                    showHideJSON.click()
-                },2000)
-            })
-                
-            //debugger
-        }
-
+        console.log('meeTree UI div found, populating it ...')
+        metr.loginUI()
     }
 }
 
-metr.fun={}
-metr.tCall={} // time to callback
+metr.loginUI=_=>{
+    metr.div.innerHTML='<p>Username:<input id="uname"></p><p>Password: <input type="password"></p><p><button id="loginBt">Login</button></p>'
+    document.getElementById('loginBt').onclick=async _=>{
+        alert('where should I post this? \nShould I pass uname and passwd in POST body?')
+        let apiUrl = ''
 
-metr.get=function(url,fun){
-    var id = Math.random().toString().slice(2)
-    fun=fun||console.log
-    fun2=function(x){
-        metr.tCall[id]=false // stop timout
-        fun(x)
+        /*
+        fetch(`url`,{
+            method:"POST",
+            mode:"no-cors",
+            body=`uname=${uname.value}&passwd=${passwd.value}`
+        })
+        */
     }
-    var s = document.createElement('script')
-    s.id='fun_'+id
-    s.src=url+'?callback=metr.fun['+id+']'
-    metr.fun[id]=fun2
-    metr.tCall[id]=true
-    setTimeout(function(){
-       if(metr.tCall[id]){
-           fun({error:'your session timed out, you need to <a href="" target="_blank">login again</a>'})
-           var testResults=metr.div.querySelector('#testResults')
-           ifr=document.createElement('iframe')
-           ifr.src='http://test.metree.lgs.littlegreensoftware.com:8888/login'
-           ifr.width="100%"
-           ifr.height=500
-           testResults.appendChild(ifr)
-       }
-    },2000)
-    document.head.appendChild(s)
 }
 
-metr.date=Date()
+
 
 window.onload=metr
