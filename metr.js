10868d64ce1a3ec7a3e86376ecf49eb2d542cdf8
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-22 12:51:10 -0400

    Update metr.js

--- metr.js
+++ metr.js
@@ -12,7 +12,7 @@ metr=function(){
 metr.loginUI=_=>{
     metr.div.innerHTML='<p>Username: <input id="uname"></p><p>Password: <input type="password"></p><p><button id="loginBt">Login</button></p>'
     document.getElementById('loginBt').onclick=async _=>{
-        alert('Where should I post this? \nShould I pass uname and passwd in POST body?')
+        alert('Where should I POST this? \nShould I pass uname and passwd in POST body?')
         let apiUrl = ''
 
         /*
