2c0c1203ce0e4ca71107f8a09076f3cc90cf1bd3
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-06 12:45:39 -0400

    env variables

--- metr.js
+++ metr.js
@@ -6,22 +6,34 @@ metr=function(){
     if(metr.div){
         console.log('meeTree UI div found, populating it ...')
         metr.loginUI()
+        console.log('clicking in 1 sec ...')
+        setTimeout(loginBt.onclick,1000)
     }
 }
 
 metr.loginUI=_=>{
-    metr.div.innerHTML='<p>Username:<input id="uname"></p><p>Password: <input type="password"></p><p><button id="loginBt">Login</button></p>'
+    let parms=JSON.parse(localStorage.metree)
+    parms.uname=parms.uname||''
+    parms.passwd=parms.passwd||''
+    metr.div.innerHTML=`<p>Username:<input id="uname" value="${parms.uname}"></p><p>Password: <input id="passwd" type="password" value="${parms.passwd}"></p><p><button id="loginBt">Login</button></p>`
     document.getElementById('loginBt').onclick=async _=>{
-        alert('where should I post this? \nShould I pass uname and passwd in POST body?')
-        let apiUrl = ''
-
-        /*
-        fetch(`url`,{
+        //alert('where should I post this? \nShould I pass uname and passwd in POST body?')
+        fetch('https://metree.familyhistory.duke.edu/v2/login',{
             method:"POST",
-            mode:"no-cors",
-            body=`uname=${uname.value}&passwd=${passwd.value}`
+            mode:'no-cors',
+            cache: 'no-cache',
+            //referrer: "no-referrer",
+            //redirect: "follow",
+            credentials: "include",
+            body:`username=${uname.value}&password=${passwd.value}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                //'Content-Type': 'application/json',
+            }
+        }).then(x=>{
+            console.log(x)
+            //debugger
         })
-        */
     }
 }
 
